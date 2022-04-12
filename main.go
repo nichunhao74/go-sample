b2e3ddb8e1144ea8942d41b5463cf1a0de30f838
nichunhao74 <nichunhao74@gmail.com>
2022-04-12 03:12:41 +0000

    aa

--- main.go
+++ main.go
@@ -2,6 +2,8 @@ package main
 
 import (
 	"fmt"
+	"log"
+	"net/http"
 	"time"
 )
 
@@ -9,4 +11,11 @@ func main() {
 	t := time.Now()
 	fmt.Println("hello world")
 	fmt.Println("今の時刻：" + t.String())
+
+	port := "8080"
+
+	http.Handle("/", http.FileServer(http.Dir("root/"))) // ①
+	log.Printf("Server listening on http://localhost:%s/", port)
+	log.Print(http.ListenAndServe(":"+port, nil))
+
 }
